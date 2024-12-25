9050fa87917d0455f5f147b99a5af4f406a73f46
brinkpku <brink_no_reply@pku.edu.cn>
2024-12-25 17:48:25 +0800

    add prefix sum solution for 437

--- binarytree/lc_437_m.go
+++ binarytree/lc_437_m.go
@@ -25,3 +25,25 @@ func pathSum1(root *TreeNode, targetSum int) int {
 	}
 	return pathSum(root.Left, targetSum) + countPath(root, targetSum) + pathSum(root.Right, targetSum)
 }
+
+// 前缀和+dfs遍历
+func pathSum(root *TreeNode, targetSum int) int {
+	var traverse func(*TreeNode)
+	ans := 0
+	sum := 0
+	hashes := map[int]int{0: 1}
+	traverse = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		sum += node.Val
+		ans += hashes[sum-targetSum]
+		hashes[sum]++
+		traverse(node.Left)
+		traverse(node.Right)
+		hashes[sum]--
+		sum -= node.Val
+	}
+	traverse(root)
+	return ans
+}
