f671ede125742dc0eae8f9be94863f768ed34920
brinkpku <brink_no_reply@pku.edu.cn>
2025-01-24 01:44:37 +0800

    more pruning for 22

--- backtrack/lc_22_m.go
+++ backtrack/lc_22_m.go
@@ -2,42 +2,36 @@ package backtrack
 
 import "strings"
 
+// 无重可重复选组合问题
 func generateParenthesis(n int) []string {
 	p := []string{"(", ")"}
 	var backtrack func()
 	track := []string{}
 	ans := []string{}
+	left, right := 0, 0
 	backtrack = func() {
-		if len(track) == 2*n {
-			if validParenthesis(track) {
+		if left < right || len(track) == 2*n {
+			if left == right {
 				ans = append(ans, strings.Join(track, ""))
 			}
 			return
 		}
 		for i := 0; i < len(p); i++ {
-			if len(track) > 0 && string(track[0]) == ")" {
-				continue
+			if p[i] == "(" {
+				left++
+			} else {
+				right++
 			}
 			track = append(track, string(p[i]))
 			backtrack()
+			if p[i] == "(" {
+				left--
+			} else {
+				right--
+			}
 			track = track[:len(track)-1]
 		}
 	}
 	backtrack()
 	return ans
-}
-
-func validParenthesis(s []string) bool {
-	leftCount := 0
-	for _, r := range s {
-		if string(r) == "(" {
-			leftCount++
-		} else {
-			if leftCount == 0 {
-				return false
-			}
-			leftCount--
-		}
-	}
-	return leftCount == 0
 }
