a11e4ca9662335671611b450192e401fad614386
brinkpku <brink_no_reply@pku.edu.cn>
2025-02-04 17:00:59 +0800

    linklist cycle

---
+++ linklist/lc_141_e.go
@@ -0,0 +1,14 @@
+package linklist
+
+// 快慢指针判断环
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
---
+++ linklist/lc_142_m.go
@@ -0,0 +1,18 @@
+package linklist
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+			return slow
+		}
+	}
+	return nil
+}
