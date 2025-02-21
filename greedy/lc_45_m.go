27c79600446ba46fa87398ba4adbd6b8e5d489aa
brinkpku <brink_no_reply@pku.edu.cn>
2025-02-21 23:52:23 +0800

    add jump 2

---
+++ greedy/lc_45_m.go
@@ -0,0 +1,46 @@
+package greedy
+
+import "math"
+
+// 动态规划做法，自顶向下，递归+备忘录
+func jump(nums []int) int {
+	var dfs func(i int) int
+	dp := make([]int, len(nums))
+	for i := range dp {
+		dp[i] = -1
+	}
+	dfs = func(i int) int {
+		if i == 0 {
+			return 0
+		}
+		if dp[i] != -1 {
+			return dp[i]
+		}
+		ans := math.MaxInt
+		for j := i - 1; j >= 0; j-- {
+			if nums[j]+j >= i {
+				ans = min(ans, dfs(j)+1)
+			}
+		}
+		dp[i] = ans
+		return ans
+	}
+	return dfs(len(nums) - 1)
+}
+
+// 动态规划做法，自底向上
+func jump2(nums []int) int {
+	dp := make([]int, len(nums))
+	for i := range dp {
+		dp[i] = math.MaxInt
+	}
+	dp[0] = 0
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if j+nums[j] >= i {
+				dp[i] = min(dp[j]+1, dp[i])
+			}
+		}
+	}
+	return dp[len(nums)-1]
+}
