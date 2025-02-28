727008486705d6386d946fdb3cc79569014098fe
brinkpku <brink_no_reply@pku.edu.cn>
2025-02-28 17:00:11 +0800

    bfs lands

--- graph/lc_200_m.go
+++ graph/lc_200_m.go
@@ -33,3 +33,48 @@ func numIslands(grid [][]byte) int {
 	}
 	return ans
 }
+
+// 广度优先遍历
+func numIslands2(grid [][]byte) int {
+	visited := make([][]bool, len(grid))
+	for i := range visited {
+		visited[i] = make([]bool, len(grid[0]))
+	}
+	ans := 0
+	qr, qc := make([]int, 0), make([]int, 0)
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] == '1' && !visited[i][j] {
+				ans++
+				qr = append(qr, i)
+				qc = append(qc, j)
+				for len(qr) != 0 {
+					r, c := qr[0], qc[0]
+					visited[r][c] = true
+					if 0 < c && grid[r][c-1] == '1' && !visited[r][c-1] {
+						qr = append(qr, r)
+						qc = append(qc, c-1)
+						visited[r][c-1] = true
+					}
+					if c < len(grid[0])-1 && grid[r][c+1] == '1' && !visited[r][c+1] {
+						qr = append(qr, r)
+						qc = append(qc, c+1)
+						visited[r][c+1] = true
+					}
+					if 0 < r && grid[r-1][c] == '1' && !visited[r-1][c] {
+						qr = append(qr, r-1)
+						qc = append(qc, c)
+						visited[r-1][c] = true
+					}
+					if r < len(grid)-1 && grid[r+1][c] == '1' && !visited[r+1][c] {
+						qr = append(qr, r+1)
+						qc = append(qc, c)
+						visited[r+1][c] = true
+					}
+					qr, qc = qr[1:], qc[1:]
+				}
+			}
+		}
+	}
+	return ans
+}
