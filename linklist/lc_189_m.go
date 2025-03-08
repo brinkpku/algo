d33fa2c2f52605aab353a2fbb4d6c085f292e122
brinkpku <brink_no_reply@pku.edu.cn>
2025-03-09 01:20:24 +0800

    rotate array

---
+++ linklist/lc_189_m.go
@@ -0,0 +1,14 @@
+package linklist
+
+func rotate(nums []int, k int) {
+	k %= len(nums)
+	reverse(nums, 0, len(nums)-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, len(nums)-1)
+}
+
+func reverse(nums []int, i, j int) {
+	for ; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+}
