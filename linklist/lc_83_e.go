2abd96c7939cfa4cfdd52c7bf92b755bac17e7f2
brink <brink@pku.edu.cn>
2025-04-15 23:30:21 +0800

    iterate to delete duplicates

--- linklist/lc_83_e.go
+++ linklist/lc_83_e.go
@@ -12,3 +12,16 @@ func deleteDuplicates(head *ListNode) *ListNode {
 	head.Next = deleteDuplicates(p.Next)
 	return head
 }
+
+// 迭代删除重复节点
+func deleteDuplicates2(head *ListNode) *ListNode {
+	p := head
+	for p != nil && p.Next != nil {
+		if p.Val != p.Next.Val {
+			p = p.Next
+		} else {
+			p.Next = p.Next.Next
+		}
+	}
+	return head
+}
